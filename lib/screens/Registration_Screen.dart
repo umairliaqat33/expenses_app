ff4b0050032057b5bca99a9fa20d21f3140b1583
umair <umairliaqat552@gmail.com>
2022-05-19 17:52:13 +0500

    added a loader while making account

--- lib/screens/Registration_Screen.dart
+++ lib/screens/Registration_Screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'welcome_screen.dart';
 import 'package:expenses_app/models/user_model.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({Key? key}) : super(key: key);
@@ -22,7 +23,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final L_name_Controller = TextEditingController();
   final F_name_Controller = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  // bool showSpinner=false;
+  bool showSpinner=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,186 +41,192 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
         ),
       ),
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Hero(
-                  tag: 'logo',
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('assets/images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Hero(
+                    tag: 'logo',
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('assets/images/logo.png'),
+                    ),
                   ),
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      // this will be checking if we have any value in it or not?
-                      return "First name is required";
-                    }
-                    return null;
-                  },
-                  textInputAction: TextInputAction.next,
-                  cursorColor: Colors.green,
-                  keyboardType: TextInputType.name,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black),
-                  controller: F_name_Controller,
-                  decoration: kMessageTextFieldDecoration.copyWith(
-                    hintText: 'Enter Your First Name',
-                    icon: Icon(Icons.account_circle),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        // this will be checking if we have any value in it or not?
+                        return "First name is required";
+                      }
+                      return null;
+                    },
+                    textInputAction: TextInputAction.next,
+                    cursorColor: Colors.green,
+                    keyboardType: TextInputType.name,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black),
+                    controller: F_name_Controller,
+                    decoration: kMessageTextFieldDecoration.copyWith(
+                      hintText: 'Enter Your First Name',
+                      icon: Icon(Icons.account_circle),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      // this will be checking if we have any value in it or not?
-                      return "First name is required";
-                    }
-                    if (value == F_name_Controller.text) {
-                      return "First and last name can not b same";
-                    }
-                    return null;
-                  },
-                  textInputAction: TextInputAction.next,
-                  cursorColor: Colors.green,
-                  keyboardType: TextInputType.name,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black),
-                  controller: L_name_Controller,
-                  decoration: kMessageTextFieldDecoration.copyWith(
-                    hintText: 'Enter Your Last Name',
-                    icon: Icon(Icons.account_circle),
+                  SizedBox(
+                    height: 8.0,
                   ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      // this will be checking if we have any value in it or not?
-                      return "Field required";
-                    }
-                    if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                        .hasMatch(value)) {
-                      // this will be checking whether the value in it is an email or not?
-                      return "Enter a valid email";
-                    }
-                    return null;
-                  },
-                  textInputAction: TextInputAction.next,
-                  cursorColor: Colors.green,
-                  keyboardType: TextInputType.emailAddress,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black),
-                  controller: emailController,
-                  decoration: kMessageTextFieldDecoration.copyWith(
-                    hintText: 'Enter Your Email',
-                    icon: Icon(Icons.email),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        // this will be checking if we have any value in it or not?
+                        return "First name is required";
+                      }
+                      if (value == F_name_Controller.text) {
+                        return "First and last name can not b same";
+                      }
+                      return null;
+                    },
+                    textInputAction: TextInputAction.next,
+                    cursorColor: Colors.green,
+                    keyboardType: TextInputType.name,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black),
+                    controller: L_name_Controller,
+                    decoration: kMessageTextFieldDecoration.copyWith(
+                      hintText: 'Enter Your Last Name',
+                      icon: Icon(Icons.account_circle),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextFormField(
-                  validator: (value) {
-                    RegExp regex = new RegExp(r"^.{6,}$");
-                    if (value!.isEmpty) {
-                      return "Field is required";
-                    }
-                    if (!regex.hasMatch(value)) {
-                      return "Password must contain 6 characters minimum";
-                    }
-                    return null;
-                  },
-                  textInputAction: TextInputAction.done,
-                  cursorColor: Colors.green,
-                  textAlign: TextAlign.center,
-                  obscureText: true,
-                  style: TextStyle(color: Colors.black),
-                  controller: passController,
-                  decoration: kMessageTextFieldDecoration.copyWith(
-                    hintText: 'Enter Your Password',
-                    icon: Icon(Icons.key),
+                  SizedBox(
+                    height: 8.0,
                   ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Field is required";
-                    }
-                    if (value != passController.text) {
-                      return "Password do not match try again";
-                    }
-                    return null;
-                  },
-                  textInputAction: TextInputAction.done,
-                  cursorColor: Colors.green,
-                  textAlign: TextAlign.center,
-                  obscureText: true,
-                  style: TextStyle(color: Colors.black),
-                  controller: C_pass_controller,
-                  decoration: kMessageTextFieldDecoration.copyWith(
-                    hintText: 'Confirm Password',
-                    icon: Icon(Icons.key),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        // this will be checking if we have any value in it or not?
+                        return "Field required";
+                      }
+                      if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                          .hasMatch(value)) {
+                        // this will be checking whether the value in it is an email or not?
+                        return "Enter a valid email";
+                      }
+                      return null;
+                    },
+                    textInputAction: TextInputAction.next,
+                    cursorColor: Colors.green,
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black),
+                    controller: emailController,
+                    decoration: kMessageTextFieldDecoration.copyWith(
+                      hintText: 'Enter Your Email',
+                      icon: Icon(Icons.email),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  child: Material(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                    elevation: 5.0,
-                    child: MaterialButton(
-                      onPressed: () {
-                        SignUp(emailController.text, passController.text);
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      },
-                      splashColor: null,
-                      minWidth: 200.0,
-                      height: 42.0,
-                      child: Text(
-                        'Register',
-                        style: TextStyle(color: Colors.white),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      RegExp regex = new RegExp(r"^.{6,}$");
+                      if (value!.isEmpty) {
+                        return "Field is required";
+                      }
+                      if (!regex.hasMatch(value)) {
+                        return "Password must contain 6 characters minimum";
+                      }
+                      return null;
+                    },
+                    textInputAction: TextInputAction.done,
+                    cursorColor: Colors.green,
+                    textAlign: TextAlign.center,
+                    obscureText: true,
+                    style: TextStyle(color: Colors.black),
+                    controller: passController,
+                    decoration: kMessageTextFieldDecoration.copyWith(
+                      hintText: 'Enter Your Password',
+                      icon: Icon(Icons.key),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Field is required";
+                      }
+                      if (value != passController.text) {
+                        return "Password do not match try again";
+                      }
+                      return null;
+                    },
+                    textInputAction: TextInputAction.done,
+                    cursorColor: Colors.green,
+                    textAlign: TextAlign.center,
+                    obscureText: true,
+                    style: TextStyle(color: Colors.black),
+                    controller: C_pass_controller,
+                    decoration: kMessageTextFieldDecoration.copyWith(
+                      hintText: 'Confirm Password',
+                      icon: Icon(Icons.key),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    child: Material(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      elevation: 5.0,
+                      child: MaterialButton(
+                        onPressed: () {
+                          setState(() {
+                            showSpinner=true;
+                          });
+                          SignUp(emailController.text, passController.text);
+                          FocusManager.instance.primaryFocus?.unfocus();
+                        },
+                        splashColor: null,
+                        minWidth: 200.0,
+                        height: 42.0,
+                        child: Text(
+                          'Register',
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Already have an account? '),
-                    TextButton(
-                      style: ButtonStyle(
-                          splashFactory: NoSplash
-                              .splashFactory //removing onclick splash color
-                          ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginScreen()));
-                      },
-                      child: Text("LogIn"),
-                    ),
-                  ],
-                ),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Already have an account? '),
+                      TextButton(
+                        style: ButtonStyle(
+                            splashFactory: NoSplash
+                                .splashFactory //removing onclick splash color
+                            ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginScreen()));
+                        },
+                        child: Text("LogIn"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
